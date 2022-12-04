ddebb6afdfac5bd7b2901ce8d419d0db4316afd9
Dmitry Bogdanov <dimatribal4@gmail.com>
2022-12-04 18:12:33 +0800

    final task solution

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,17 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client:{name, phone}, order:{address, sum}, goods:[title, count]};
+   
+    data.order.address = "ул. " + (address.street) + ", дом " + (address.house) + ", " + (address.entrance) + " подъезд, " + (address.floor) + " этаж, кв " + (address.flat);
+    data.client = ${name} + " " + ${phone};
+    data.order.sum = sum;
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1 ) {
+        data.goods.push({goods[i].title, goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
