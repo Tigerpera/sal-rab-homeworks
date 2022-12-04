0915ad117d6df59a55b84427e789dd953a97daf4
Dmitry Bogdanov <dimatribal4@gmail.com>
2022-12-04 21:23:09 +0800

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,17 +34,16 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client:{name, phone}, order:{address, sum}, goods:[title, count]};
-   
-    data.order.address = "ул. " + (address.street) + ", дом " + (address.house) + ", " + (address.entrance) + " подъезд, " + (address.floor) + " этаж, кв " + (address.flat);
-    data.client = ${name} + " " + ${phone};
-    data.order.sum = sum;
-    let countOfGoods = goods.length;
-    for (let i = 0; i < countOfGoods; i += 1 ) {
-        data.goods.push({goods[i].title, goods[i].count});
+      let data = {client: "", order: {address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat, sum: sum}, goods: []};
+
+        data.client = name + " " + phone;
+        data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat;
+        data.order.sum = sum;
+     let countOfGoods = goods.length;
+     for (let i = 0; i < countOfGoods; i ++) {
+      data.goods.push ({"title": goods[i].title, "count": goods[i].count});
+     } 
+        let jsonData = JSON.stringify({data:data});
+
+        return jsonData;
     }
-
-    let jsonData = JSON.stringify({data});
-
-    return jsonData;
-}
